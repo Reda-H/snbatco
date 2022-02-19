e7eaa6dc86e5f475db42e6d116bbe648fced6bdb
Reda Herradi <Reda.Herradi@nestle.com>
2022-02-19 14:15:49 +0100

    added ready made components

--- pages/contact.js
+++ pages/contact.js
@@ -1,7 +1,8 @@
 import React from "react";
+import ContactCard from "../components/ContactCard";
 
 function Contact() {
-  return <div>Contact</div>;
+  return <ContactCard />;
 }
 
 export default Contact;
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,18 @@
-import { Box, List, ListIcon, ListItem } from "@chakra-ui/layout";
+import {
+  Box,
+  Flex,
+  Grid,
+  GridItem,
+  List,
+  ListIcon,
+  ListItem,
+} from "@chakra-ui/layout";
 import { BiRightArrow } from "react-icons/bi";
 import { IoIosArrowForward } from "react-icons/io";
 import AboutCard from "../components/AboutCard";
+import Carousel from "../components/Carousel";
+import Hero from "../components/Hero";
+import ProjectCard from "../components/ProjectCard";
 
 const sidebarItem = [
   {
@@ -20,22 +31,46 @@ function Home() {
     <Box>
       <Box>
         {/* Presentation Area */}
-        <AboutCard />
+        {/* <AboutCard /> */}
+        {/* <Hero /> */}
+        <Carousel />
       </Box>
       <Box>
-        {/* Mosaic Area */}
-        {/* Menu Side */}
-        <Box w={"10%"} bg={"black"} height={"500px"} color={"white"}>
-          <List spacing={3}>
-            {sidebarItem.map((item) => (
+        <Flex direction={"row"}>
+          {/* Mosaic Area */}
+          {/* Menu Side */}
+          <Box w={"10%"} bg={"black"} height={"500px"} color={"white"}>
+            <List spacing={3}>
+              {sidebarItem.map((item) => (
+                <ListItem key={() => Math.random()}>
+                  <ListIcon as={IoIosArrowForward} color="white" />
+                  {item.title}
+                </ListItem>
+              ))}
+            </List>
+          </Box>
+          {/* Tab Component */}
+          <Box w={"90%"} padding={"2rem"}>
+            <List>
               <ListItem key={() => Math.random()}>
-                <ListIcon as={IoIosArrowForward} color="white" />
-                {item.title}
+                <Grid
+                  templateRows={"repeat(4, 1fr)"}
+                  templateColumns={"repeat(6, 1fr)"}
+                >
+                  <GridItem rowSpan={1} colSpan={2}>
+                    <ProjectCard />
+                  </GridItem>
+                  <GridItem rowSpan={1} colSpan={2}>
+                    <ProjectCard />
+                  </GridItem>
+                  <GridItem rowSpan={1} colSpan={2}>
+                    <ProjectCard />
+                  </GridItem>
+                </Grid>
               </ListItem>
-            ))}
-          </List>
-        </Box>
-        {/* Tab Component */}
+            </List>
+          </Box>
+        </Flex>
         {/* Mosaic per Tab */}
       </Box>
     </Box>
